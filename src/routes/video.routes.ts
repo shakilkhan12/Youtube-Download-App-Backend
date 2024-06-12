75cd457099db8cb55c092b5bef05d20542eeae15
shakilkhan12 <shakilkhan621@gmail.com>
2024-06-12 11:43:42 +0500

    Just add a comment

--- src/routes/video.routes.ts
+++ src/routes/video.routes.ts
@@ -3,6 +3,7 @@
 import { VideoController } from "@/controllers";
 import { Router } from "express";
 const router  = Router();
+// Routes
 router.get('/video-details', VideoController.videoDetails)
 router.get('/download', VideoController.downloadVideo)
 export { router as videoRouter}
